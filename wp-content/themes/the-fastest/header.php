796ab3ef491643b70376971685f4d88e441b9abf
Alejandro Sanchez <aalejo@gmail.com>
2017-09-19 19:00:50 +0000

    tag manager

--- wp-content/themes/the-fastest/header.php
+++ wp-content/themes/the-fastest/header.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PGGRR6');</script>
+        <!-- End Google Tag Manager -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo wp_title(); ?></title>
         <?php wp_head(); ?>
         <?php wpas_head(); ?>
     </head>
     <body <?php echo body_class(); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGGRR6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="bg-sketch"></div>
     <?php wpas_critical_head(); ?>
     <?php get_template_part('partials/navbar','main'); ?>
