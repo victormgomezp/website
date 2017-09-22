7f26ec3476ce26d94b8924e6704c3775001c02ab
Alejandro Sanchez <aalejo@gmail.com>
2017-09-22 16:49:34 +0000

    page backgorund color

--- wp-content/themes/the-fastest/page.php
+++ wp-content/themes/the-fastest/page.php
@@ -1,5 +1,6 @@
 <?php 
-    get_header(); 
+    /* Template Name: Visual Composer Ready */
+    get_header('visualcomposer'); 
     $args = wpas_get_view_data();
 ?>
   <?php echo do_shortcode($args['wp_query']->post_content); ?>
