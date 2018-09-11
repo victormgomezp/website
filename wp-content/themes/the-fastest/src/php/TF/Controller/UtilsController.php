67a5d3aaea0fd5bd537cc43d051741a89de60d8c
Alejandro Sanchez <aalejo@gmail.com>
2018-09-11 09:51:53 +0000

    updated website, ready for production

--- wp-content/themes/the-fastest/src/php/TF/Controller/UtilsController.php
+++ wp-content/themes/the-fastest/src/php/TF/Controller/UtilsController.php
@@ -15,7 +15,7 @@ class UtilsController{
         if($backgroundColor) $styles .= 'background-color: '.$backgroundColor.';';
         
         $backgroundImage = get_field('body_background_image', $postId);
-        //debug(wp_get_attachment_url($backgroundImage));
+        debug($backgroundImage);
         if(isset($backgroundImage)){
             if(isset($backgroundImage['url'])) $styles .= "background-image: url('".$backgroundImage['url']."');";
             else $styles .= "background-image: url('".wp_get_attachment_url($backgroundImage)."');";
