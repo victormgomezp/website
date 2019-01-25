068f8b7356e77738a7d3be0acec794849f957f8d
Alejandro Sanchez <aalejo@gmail.com>
2019-01-25 04:10:25 +0000

    debugging

--- wp-content/themes/the-fastest/src/php/TF/ThemeManager.php
+++ wp-content/themes/the-fastest/src/php/TF/ThemeManager.php
@@ -62,7 +62,7 @@ class ThemeManager{
     }
     function wpse_184163_disable_canonical_front_page( $redirect ) {
         $front_page = get_option( 'page_on_front' );
-        if ( is_page() && $front_page) {
+        if ((is_page() || is_front_page()) && $front_page) {
             $city = get_query_var('city');
             $this->_debug($city);
             if ( is_page( $front_page ) && !empty($city))
