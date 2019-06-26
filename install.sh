bb31150473173efc98fc7d38193d246422c527c7
Alejandro Sanchez <aalejo@gmail.com>
2019-06-26 19:17:46 +0000

    faijnkfds

---
+++ install.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+red='\e[31m'
+cyan='\e[33m'
+blue='\e[34m'
+bgRed='\e[41m'
+bgBlue='\e[44m'
+bgCyan='\e[46m'
+
+fullpath=${PWD}
+baseSiteDirectory=$(echo $fullpath | sed 's/.*workspace//g')
+
+echo "================================================================="
+echo "$blue \e[0m WordPress Installer!!"
+echo "================================================================="
+print_error () {
+    echo "$bgRed Error!\e[0m $red $1\e[0m"
+}
+print_info () {
+    echo "$bgBlue $1\e[0m"
+}
+print_hint () {
+    echo "$bgBlue Hint:\e[0m $1"
+}
+print_question () {
+    echo "$bgCyan input>\e[0m $cyan $1 \e[0m"
+}
+
+if [ -f ./.env ]; then
+        print_info "Loading (.env) environment file..."
+        . ./.env
+    else
+        print_error "No .env file was found"
+        exit 1
+fi
+
+git --version > /dev/null
+GIT_IS_AVAILABLE=$?
+if [ $GIT_IS_AVAILABLE -eq 0 ];
+    then echo ""
+    else
+        print_error "Git is required, but not installed"
+        exit 1
+fi
+
+composer --version > /dev/null
+COMPOSER_IS_AVAILABLE=$?
+if [ $COMPOSER_IS_AVAILABLE -eq 0 ];
+    then echo ""
+    else
+        print_error "Composer is required, but not installed"
+        exit 1
+fi
+
+if [ -d "./wp-content" ];
+    then
+        print_info "Wordpress installation was found, proceeding..."
+    else
+        print_error "No wordpress installation found"
+        print_hint "Make sure you have a wp-content folder in the current directory:"
+        ls -a
+        exit 1
+fi
+
+echo ""
+if [ -d "./vendor" ];
+    then
+        print_info "/vendor folder found, udating packages..."
+        composer update
+    else
+        print_info "NO /vendor folder found, installing packages from scratch..."
+        composer install
+fi
+
+wp --info > /dev/null
+WP_IS_AVAILABLE=$?
+if [ $WP_IS_AVAILABLE -eq 0 ];
+    then echo ""
+    else
+        print_info "Installing wordpress cli..."
+        curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+        chmod +x wp-cli.phar
+        sudo mv wp-cli.phar /usr/local/bin/wp
+fi
+
+if [ -d "./wp-includes" ]; then
+        if [ "$1" == "--force" ]; then
+                print_info "WordPress is already installed, proceeding with the rest of the installation..."
+    	    else
+                print_info "WordPress is already installed"
+                exit 1
+        fi
+    else
+        # download the WordPress core files
+        wp core download
+fi
+
+# create the wp-config file with our standard setup
+wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'DISALLOW_FILE_EDIT', true );
+PHP
+
+echo ""
+if [ -f "./wp-config.php" ];
+    then
+        print_info "wp-config.php successfully created..."
+    else
+        print_error "The wp-config.php file could not be created, make sure your database credentials are ok."
+        exit 1
+fi
+
+# parse the current directory name
+wp core install --url="$SITE_URL" --title="$SITE_NAME" --admin_user="$SITE_USER" --admin_password="$SITE_PASS" --admin_email="$SITE_EMAIL"
+
+echo "================================================================="
+print_info "Installation is complete."
+echo "================================================================="
+
+exit 0
--- wp-content/themes/the-fastest/functions.php
+++ wp-content/themes/the-fastest/functions.php
@@ -3,15 +3,15 @@
     if(!defined('ABSPATH')) define('ABSPATH', dirname(__FILE__) . '/');
     if(!defined('PUBLICPATH')) define('PUBLICPATH', get_site_url() . '/wp-content/themes/the-fastest/public/');
     require ABSPATH . 'vendor/autoload.php';
-    
+
     if(!file_exists(ABSPATH.'wp-content/themes/the-fastest/public/')) throw new Exception('There is no public folder in '.ABSPATH.'wp-content/themes/the-fastest/, $ npm run build');
-    
+
     use TF\ThemeManager;
     $themeManager = new ThemeManager();
-    
+
     use TF\ThemeAdminSettings;
     $settings = new ThemeAdminSettings();
-    
+
     use WPAS\Performance\WPASAsyncLoader;
     $publicPath = get_stylesheet_directory_uri();
     //print_r($publicPath); die();
@@ -22,13 +22,13 @@
         'force-jquery' => true,
         'minify-html' => UGLIFY_HTML,
         'styles' => [
-            "page" => [ 
+            "page" => [
                 "template:page-landing-maker1.php" => 'landing.css',
                 "all" => 'index.css'
             ],
-            "custom-post" => [ 
+            "custom-post" => [
                 "template:page-landing-vc.php" => 'landing.css',
-                "all" => 'index.css' 
+                "all" => 'index.css'
             ],
             "404" => ["all" => 'index.css']
         ],
@@ -62,68 +62,68 @@
                 ]
             ]
         ]);
-        
+
     use \WPAS\Controller\WPASController;
     $controller = new WPASController([
         'namespace' => 'TF\\Controller\\'
     ]);
-    
+
     $controller->route([ 'slug' => 'Template:page-landing-maker1.php', 'controller' => 'Landing:renderLanding']);
     $controller->route([ 'slug' => 'Template:page-landing-vc.php', 'controller' => 'Landing:renderLanding']);
     $controller->route([ 'slug' => 'Template:page-default.php', 'controller' => 'Landing:renderLanding']);
     $controller->route([ 'slug' => 'Single:landing', 'controller' => 'Landing:renderLanding']);
-    
+
     $controller->route([ 'slug' => 'single:job', 'controller' => 'General:renderSingleJob']);
     $controller->route([ 'slug' => 'jobs', 'controller' => 'General:renderJobs']);
     $controller->route([ 'slug' => 'trabaja', 'controller' => 'General:renderJobs']);
-    
+
     $controller->route([ 'slug' => 'blog:blog', 'controller' => 'Blog']);
     $controller->route([ 'slug' => 'blog:blog-en-espanol', 'controller' => 'Blog']);
-    
+
     $controller->route([ 'slug' => 'the-academy', 'controller' => 'General']);
     $controller->route([ 'slug' => 'academia', 'controller' => 'General:renderTheAcademy']);
-    
+
     $controller->route([ 'slug' => 'apply', 'controller' => 'General']);
     $controller->route([ 'slug' => 'aplica', 'controller' => 'General:renderApply']);
-    
+
     $controller->route([ 'slug' => 'the-program', 'controller' => 'General']);
     $controller->route([ 'slug' => 'programa', 'controller' => 'General:renderTheProgram']);
 
     $controller->route([ 'slug' => 'Single:course', 'controller' => 'General:renderTheProgram']);
 
     $controller->route([ 'slug' => 'venezuela', 'controller' => 'General:renderTheProgram']);
-    
+
     $controller->route([ 'slug' => 'partners', 'controller' => 'General:renderPartners']);
     $controller->route([ 'slug' => 'aliados', 'controller' => 'General:renderPartners']);
 
     $controller->route([ 'slug' => 'Course', 'controller' => 'Course']);
-    
+
     $controller->route([ 'slug' => 'calendar', 'controller' => 'Calendar']);
     $controller->route([ 'slug' => 'calendario', 'controller' => 'Calendar:renderCalendar']);
-    
+
     $controller->route([ 'slug' => 'pricing', 'controller' => 'General']);
     $controller->route([ 'slug' => 'precio', 'controller' => 'General:renderPricing']);
-    
-    
+
+
     $controller->route([ 'slug' => 'Single:post', 'controller' => 'Blog']);
-    
+
     $controller->route([ 'slug' => 'Category:news', 'controller' => 'Blog']);
     $controller->route([ 'slug' => 'Tag:all', 'controller' => 'Blog:renderTag']);
-    
+
     $controller->route([ 'slug' => 'Single:event', 'controller' => 'Event']);
-    
+
     $controller->route([ 'slug' => 'Single:location', 'controller' => 'General:renderLocation']);
-    
+
     $controller->route([ 'slug' => 'home', 'controller' => 'General']);
     $controller->route([ 'slug' => 'inicio', 'controller' => 'General:renderHome']);
-    
+
     $controller->route([ 'slug' => 'Template:page.php', 'controller' => 'Landing:renderLanding']);
-    
+
     $controller->routeAjax([ 'slug' => 'all', 'controller' => 'General:newsletter_signup', 'scope' => 'public' ]);
     $controller->routeAjax([ 'slug' => 'all', 'controller' => 'General:download_syllabus', 'scope' => 'public' ]);
     $controller->routeAjax([ 'slug' => 'all', 'controller' => 'General:get_upcoming_event', 'scope' => 'public' ]);
     $controller->routeAjax([ 'slug' => 'apply', 'controller' => 'General:get_incoming_dates' ]);
-    
+
     use \WPAS\Controller\WPASAPIController;
     $api = new WPASAPIController([
         'namespace' => 'TF\\Controller\\',
@@ -131,7 +131,7 @@
         'version' => 1,
         'allow-origin' => '*'
     ]);
-    
+
     $api->get(['path' => '/events', 'controller' => 'APIController:getAllEvents']);
     $api->get(['path' => '/prices', 'controller' => 'APIController:getPrices']);
     $api->get(['path' => '/locations', 'controller' => 'APIController:getAllLocations']);
@@ -141,10 +141,10 @@
     $api->get(['path' => '/hook/sync_profiles', 'controller' => 'APIController:syncProfiles']);
     $api->post(['path' => '/apply/request_info', 'controller' => 'APIController:requestMoreInfo']);
     $api->post(['path' => '/apply/request_tour', 'controller' => 'APIController:requestTour']);
-    
-    
+
+
     use \WPAS\Types\PostTypesManager;
-    
+
     $postTypeManager = new PostTypesManager([
         'namespace'=>'\TF\Types\\'
         ]);
@@ -156,10 +156,10 @@
 //    $postTypeManager->newType(['type'=>'event', 'class' => 'EventPostType'])->register();
     $postTypeManager->newType(['type'=>'landing', 'class' => 'LandingPostType'])->register();
     $postTypeManager->newType(['type'=>'partner', 'class' => 'PartnerPostType'])->register();
-    
+
     use TF\ActiveCampaign\ACAPI;
     ACAPI::start();
-    
+
     use WPAS\GravityForm\WPASGravityForm;
     if ( class_exists( 'GFCommon' ) )
     {
@@ -172,14 +172,14 @@
                 ['type' => 'alert-message', 'label' => 'Alert Message']
             ]
         ]);
-        
+
     }
-    
+
     $gfManager = new WPAS\Language\WPASLanguages([
         'languages-directory' => ABSPATH.'wp-content/themes/the-fastest/src/php/languages/'
     ]);
-    
+
     $vcSettings = new TF\VCSettings();
-    
+
     use WPAS\Messaging\WPASAdminNotifier;
     WPASAdminNotifier::loadTransientMessages();
